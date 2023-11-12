ccd5079d70230a1d34132b5dab43e3d19ab2f740
Yadidiah <94169481+YADIDidiah24@users.noreply.github.com>
2023-11-12 23:20:28 +0400

    Update countLogs.sh

--- countLogs.sh
+++ countLogs.sh
@@ -6,12 +6,12 @@ log_file_path="/path/to/your/logfile.log"  # Replace with the actual path to you
 start_time=$(date -d "1 hour ago" "+%Y-%m-%dT%H:%M:%S")
 
 # Count the occurrences of different log types from the last hour
-num_incoming_requests=$(awk -v start_time="$start_time" '/IncomingRequest/ && $1 >= start_time { count++ } END { print count }' "$log_file_path")
-num_failed_login=$(awk -v start_time="$start_time" '/FailedLogin/ && $1 >= start_time { count++ } END { print count }' "$log_file_path")
-num_successful_login=$(awk -v start_time="$start_time" '/SuccessfulLogin/ && $1 >= start_time { count++ } END { print count }' "$log_file_path")
-num_unauthorized_access=$(awk -v start_time="$start_time" '/UnauthorizedAccess/ && $1 >= start_time { count++ } END { print count }' "$log_file_path")
-num_authorized_access=$(awk -v start_time="$start_time" '/AuthorizedAccess/ && $1 >= start_time { count++ } END { print count }' "$log_file_path")
-num_suspicious_download=$(awk -v start_time="$start_time" '/SuspiciousDownload/ && $1 >= start_time { count++ } END { print count }' "$log_file_path")
+num_incoming_requests=$(awk -v start_time="$start_time" '/IncomingRequest/ && $1 >= start_time { count++ } END { print count+0 }' "$log_file_path")
+num_failed_login=$(awk -v start_time="$start_time" '/FailedLogin/ && $1 >= start_time { count++ } END { print count+0 }' "$log_file_path")
+num_successful_login=$(awk -v start_time="$start_time" '/SuccessfulLogin/ && $1 >= start_time { count++ } END { print count+0 }' "$log_file_path")
+num_unauthorized_access=$(awk -v start_time="$start_time" '/UnauthorizedAccess/ && $1 >= start_time { count++ } END { print count+0 }' "$log_file_path")
+num_authorized_access=$(awk -v start_time="$start_time" '/AuthorizedAccess/ && $1 >= start_time { count++ } END { print count+0 }' "$log_file_path")
+num_suspicious_download=$(awk -v start_time="$start_time" '/SuspiciousDownload/ && $1 >= start_time { count++ } END { print count+0 }' "$log_file_path")
 
 # Print the counts
 echo "Number of IncomingRequest: $num_incoming_requests"
