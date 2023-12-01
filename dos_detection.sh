09ed245ea47f7d8d37d20a7852b442236f26638d
Yadidiah <94169481+YADIDidiah24@users.noreply.github.com>
2023-12-01 09:44:42 +0400

    Update dos_detection.sh

--- dos_detection.sh
+++ dos_detection.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 log_file_path="/var/log/auth.log"  # Replace with the actual path to your log file
+dos_log="/var/log/dos.log"  # Specify the path for the DoS attack log
 threshold_requests=10000
 threshold_duration=60  # in seconds
 
@@ -22,6 +23,14 @@ requests=$(awk -v start_time="$start_time" -v current_time="$current_time" \
 if [ "$requests" -gt "$threshold_requests" ]; then
   echo "DoS attack detected! Blocking incoming traffic..."
   block_traffic
+  
+  # Extract username and IP address from the log file (adjust accordingly based on your log format)
+  username=$(awk '$NF == "Failed" { print $(NF-1) }' "$log_file_path")
+  ip_address=$(awk '$NF == "Failed" { print $(NF-3) }' "$log_file_path")
+  
+  # Log the DoS attack
+  logger -t "DoS attack" -p auth.notice "DoS attack attempt by $username from IP $ip_address"
+  
   sleep 2  # Adjust the sleep interval as needed
 fi
 
