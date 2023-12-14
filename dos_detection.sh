43dca503f3788797af7b3b243f78a78b8eb3a04a
Yadidiah <94169481+YADIDidiah24@users.noreply.github.com>
2023-12-14 15:00:31 +0400

    Update dos_detection.sh

--- dos_detection.sh
+++ dos_detection.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-log_file_path="/var/log/auth.log"  # Replace with the actual path to your log file
+log_file_path="/var/log/auth.log"  
 threshold_requests=10000
-threshold_duration=60  # in seconds
+threshold_duration=60  
 
 block_traffic() {
   # Clear existing rules for port 9080
   sudo iptables -D INPUT -p tcp --dport 9080 -j ACCEPT
   sudo iptables -D INPUT -p tcp --dport 9080 -j REJECT --reject-with icmp-port-unreachable
 
-  # Add the reject rule
-  sudo iptables -A INPUT -p tcp --dport 9080 -j REJECT --reject-with icmp-port-unreachable
+  
+  sudo iptables -A INPUT -p tcp --dport 9080 ! -s 127.0.0.1 -j REJECT --reject-with icmp-port-unreachable
 }
 
 start_time=$(date -d "-${threshold_duration} seconds" +%s)
@@ -25,8 +25,8 @@ if [ "$requests" -gt "$threshold_requests" ]; then
   logger -t DosAttempt -p auth.notice "DoS attack detected on port 9080"
 
   block_traffic
-  logger -t BlockTraffic -p auth.notice "Firewall has blocked all traffic"
-  sleep 2  # Adjust the sleep interval as needed
+  logger -t BlockTraffic -p auth.notice "Firewall has blocked external traffic on port 9080"
+  sleep 2  
 else
   echo "Dos Attack Not Detected"
   # Logging that DOS was not detected
