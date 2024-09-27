91170d95dbdda78a4ae0f2c181bd2a0f821e43b4
Yadidiah Kanaparthi <94169481+YADIDidiah24@users.noreply.github.com>
2024-09-27 12:38:35 +0400

    Delete DotTraversal.sh

--- DotTraversal.sh
+++
@@ -1,6 +0,0 @@
-logger -t DotTraversal -p auth.notice "Dot Traversal(../) Test has started and below are the results"
-cd dotdotpwn
-{ echo > ../output.txt; sudo ./dotdotpwn.pl -m http -h 127.0.0.1; } | tee -a ../output.txt
-cd ..
-tail -n 5 output.txt >> /var/log/auth.log
-logger -t DotTraversalFinished -p auth.notice "Dot Traversal(../) Test has finished"
